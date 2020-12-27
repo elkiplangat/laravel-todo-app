e82f81532d91c0bf68448b06e0853582ec330c14
kiplangat <langatkip23@yahoo.com>
2020-12-28 01:16:21 +0300

    add post route

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('index');
-});
+Route::get('/', 'App\Http\Controllers\HomeController@index');
+Route::post('/', 'App\Http\Controllers\HomeController@createTodoItem');
