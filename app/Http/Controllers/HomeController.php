842fb5a964dad848a4cf34179b177219e28c50c4
kiplangat <langatkip23@yahoo.com>
2020-12-28 01:15:37 +0300

    add create and save item method

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,10 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\TodoItem;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     //
+    public function index(){
+        $todo_items = TodoItem::all();
+        return view('index',['todo_items'=> $todo_items]);
+    }
+    public  function createTodoItem(Request $request){
+        echo $request->title;
+        echo $request->description;
+        $todo_item = new TodoItem;
+        $todo_item->title = $request->title;
+        $todo_item->description = $request->description;
+
+        $todo_item->save();
+        return redirect()->back()->with(['from_saving'=>true]);
+
+
+    }
 
 }
