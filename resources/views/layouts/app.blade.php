ffab6da198cbe7de7569c0f97777e996c8bb74c0
kiplangat <langatkip23@yahoo.com>
2020-12-27 22:38:06 +0300

    add assets to public directory

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,30 @@
+
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <title>Hello World</title>
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Nunito:400,600,700,800|Roboto:400,500,700" rel="stylesheet">
+
+    <!-- Boomerang CSS (including Bootstrap) -->
+    <link type="text/css" href="/assets/css/theme.css" rel="stylesheet">
+</head>
+<body>
+<main class="main">...</main>
+
+<!-- Bootstrap JS -->
+<script src="/assets/vendor/jquery/jquery.min.js"></script>
+<script src="/assets/vendor/popper/popper.min.js"></script>
+<script src="/assets/vendor/bootstrap/js/bootstrap.min.js"></script>
+
+<!-- FontAwesome 5 -->
+<script src="assets/vendor/fontawesome/js/fontawesome-all.min.js"></script>
+
+<!-- Boomerang JS -->
+<script src="/assets/js/theme.js"></script>
+</body>
+</html>
